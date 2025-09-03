aa738e660b8e9eaf6d05bba89617fa22c0c0c2da
Jay Patil <jaykp2526@gmail.com>
2025-09-03 11:40:35 +0530

    delete pycache and env file and add gitignore file

--- users/users.py
+++ users/users.py
@@ -1,5 +1,5 @@
 from database import engine,stock_db
-import utils
+import helper
 from fastapi import FastAPI,Depends,HTTPException,status,Response
 from sqlalchemy.orm import Session
 from . import users_models,users_schema
@@ -23,7 +23,7 @@ async def get_all_users(db:Session = Depends(stock_db)):
 @app.post("/users/createuser",status_code = status.HTTP_201_CREATED,response_model = users_schema.user_out)
 async def create_user(user:users_schema.users_create,db: Session = Depends(stock_db)):
    
-    hashed_password = utils.hash(user.password)
+    hashed_password = helper.hash(user.password)
     user.password = hashed_password
     new_user = users_models.users_table(**user.dict())
     if db.query(users_models.users_table).filter(users_models.users_table.email == user.email).first():
@@ -34,14 +34,14 @@ async def create_user(user:users_schema.users_create,db: Session = Depends(stock
     return new_user
 
 @app.get("/users/{user_id}",response_model = users_schema.user_out)
-async def get_user(user_id:int,db:Session = Depends(stock_db),user_current: users_models.users_table = Depends(utils.get_current_user)):
+async def get_user(user_id:int,db:Session = Depends(stock_db),user_current: users_models.users_table = Depends(helper.get_current_user)):
     get_user = db.query(users_models.users_table).filter(users_models.users_table.user_id == user_id).first()
     if get_user == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail = f"User with id {user_id} not found")
     return get_user
 
 @app.put("/users/{user_id}",response_model = users_schema.user_out)
-async def update_user(user_id:int,user:users_schema.users_create,db:Session = Depends(stock_db),user_current: users_models.users_table = Depends(utils.get_current_user)):
+async def update_user(user_id:int,user:users_schema.users_create,db:Session = Depends(stock_db),user_current: users_models.users_table = Depends(helper.get_current_user)):
     get_user = db.query(users_models.users_table).filter(users_models.users_table.user_id == user_id).first()
     if get_user == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail = f"User with id {user_id} not found")
@@ -53,7 +53,7 @@ async def update_user(user_id:int,user:users_schema.users_create,db:Session = De
     return get_user
 
 @app.delete("/users/{user_id}",status_code = status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id:int,db:Session = Depends(stock_db),user_current: users_models.users_table = Depends(utils.get_current_user)):
+async def delete_user(user_id:int,db:Session = Depends(stock_db),user_current: users_models.users_table = Depends(helper.get_current_user)):
     get_user = db.query(users_models.users_table).filter(users_models.users_table.user_id == user_id).first()
     if get_user == None:
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail = f"User with id {user_id} not found")
@@ -89,10 +89,10 @@ def login_form(user_form_info : OAuth2PasswordRequestForm = Depends(), db : Sess
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail = f"Invalid Credentials User Not Found")
         
-    if not utils.verify(user_form_info.password,in_db_user.password):
+    if not helper.verify(user_form_info.password,in_db_user.password):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail = f"Invalid Password")      
-    access_token = utils.create_access_token(data={"user_id":in_db_user.user_id},expires_delta = timedelta(minutes=30))
+    access_token = helper.create_access_token(data={"user_id":in_db_user.user_id},expires_delta = timedelta(minutes=30))
     
     return {"token":access_token,
             "token_type":"bearer"}
